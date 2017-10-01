0f598cb06f541e07b35343975fabb3e2f4345835
litzingr <z_wolfe@mac.com>
2017-10-02 00:40:09 +0200

    initialized for twit

---
+++ package.js
@@ -0,0 +1,28 @@
+{
+  "name": "litzingr_bot",
+  "version": "1.0.0",
+  "description": "A twitter bot tailored for Zach Litzinger",
+  "main": "bot.js",
+  "scripts": {
+    "start": "node bot.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/litzingr/litzingr_bot.git"
+  },
+  "keywords": [
+    "litzingr",
+    "bot",
+    "twitter"
+  ],
+  "author": "",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/litzingr/litzingr_bot/issues"
+  },
+  "homepage": "https://github.com/litzingr/litzingr_bot#readme",
+  "dependencies": {
+    "twit": "^2.2.9"
+  }
+}
