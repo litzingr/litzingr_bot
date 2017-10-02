1ee570b741e90448ee05b78dfb07f2c4aa4bac10
litzingr <z_wolfe@mac.com>
2017-10-02 11:41:09 +0200

    have it so tweetbot replies to tweets when talked to

--- bot.js
+++ bot.js
@@ -6,16 +6,56 @@ var config = require('./config');
 
 var T = new Twit(config);
 
-var params = {
-  q: 'rainbow',
-  count: 2
-}
+//sets up user stream
+var stream = T.stream('user');
 
-T.get('search/tweets', params, gotData);
+//when someone tweets me, call tweetRecieve
+stream.on('tweet', tweetRecieve);
 
-function gotData(err, data, response) {
-  var tweets = data.statuses;
-  for (var i = 0; i < tweets.length; i++){
-    console.log(tweets[i].text);
+function tweetCheck(err, data, response) {
+  if (err) {
+    console.log("Something went wrong\n" + err);
+  } else {
+    console.log("Tweeted: " + data.text);
   }
 }
+
+function tweetThis(text){
+  var tweet = {
+    status: text
+  }
+  T.post('statuses/update', tweet, tweetCheck);
+
+}
+
+function tweetReply(text, reply_id){
+  var tweet = {
+    status: text,
+    in_reply_to_status_id: reply_id
+  }
+  T.post('statuses/update', tweet, tweetCheck);
+
+}
+
+function tweetRecieve(eventMsg){
+  var replyto = eventMsg.in_reply_to_screen_name;
+  var from = eventMsg.user.screen_name;
+  var reply_id = eventMsg.id_str;
+  var userName = eventMsg.user.name;
+
+  console.log(replyto + " tweeted me");
+
+  //tweet back only if it's sent to me
+  if (replyto === 'litzingr_bot'){
+    tweetReply('@' + from + ' Oh hai ' + userName + '! You tweeted me!',reply_id);
+  }
+
+}
+
+
+//Example tweet
+
+// var tweet = {
+//   status: "Something happened that made me post"
+// }
+// T.post('statuses/update', tweet, tweetCheck);
